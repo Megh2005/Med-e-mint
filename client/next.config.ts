11e104ec32f5dc368ce53024565b95504daf756a
Megh2005 <megh.deb.civil27@heritageit.edu.in>
2025-12-07 12:09:20 +0530

    feat: Add initial Next.js and TypeScript configuration files.

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,4 +1,4 @@
-import type {NextConfig} from 'next';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -6,9 +6,7 @@ const nextConfig: NextConfig = {
     ignoreBuildErrors: true,
   },
   devIndicators: false,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  serverExternalPackages: ['express', 'genkit', '@genkit-ai/googleai', '@google/generative-ai'],
   images: {
     unoptimized: true,
     remotePatterns: [
